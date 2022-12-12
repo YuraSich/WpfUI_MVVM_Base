cc0adbff32e5299e5b795a370193f045a06fdd2b
Yuri Sich <metelkiny98@gmail.com>
2022-12-12 22:15:31 +0700

    exit button works

---
+++ Infrastructure/Commands/CloseAppCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WpfUI.Infrastructure.Commands.Base;
+
+namespace WpfUI.Infrastructure.Commands
+{
+    internal class CloseAppCommand : Command
+    {
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfUI.Infrastructure.Commands;
+using WpfUI.Infrastructure.Commands.Base;
+
+namespace WpfUI.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        public Command CloseAppCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            CloseAppCommand = new CloseAppCommand();
+        }
+    }
+}
